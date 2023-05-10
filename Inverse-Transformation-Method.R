1f7144e1af0e870858b2674e96e01f7eefbb6922
Sabelo Hlatshwayo <evidence.hlatshwayo@gmail.com>
2023-05-10 20:04:17 +0200

    Add the solution for Exercise 2

--- Inverse-Transformation-Method.R
+++ Inverse-Transformation-Method.R
@@ -5,3 +5,13 @@
 x = runif(1000000, 200, 210);
 hist(0.1*x-20, freq=FALSE);
 
+# Exercise 2
+# Use the inverse transformation method to simulate 10000 numbers from an exponential distribution 
+# with a rate of λ = 2. Create a histogram of the simulated values, 
+# together with the density curve of the exponential distribution.
+u = runif(1000, 0, 1);
+x = -log(1-u)/2;
+hist(x, breaks=20, freq=FALSE);
+x=seq(0, 10, length=1000);
+fx=2*exp(-2*x);
+lines(x, fx, lwd=2, col="red");
